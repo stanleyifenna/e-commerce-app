f5ffc6a32eea381fcac2ed6205706432e58ef351
Stanley Ifenna Obimkpu <obimkpustanley@gmail.com>
2024-08-01 19:44:41 +0100

    fix footer

--- src/components/front-end/Footer.tsx
+++ src/components/front-end/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#494848] py-6'>
+    <div className='bg-[#616060] py-6'>
         <p className='justify-center items-center text-center text-white'>© {new Date().getFullYear()} Doveva.</p>
     </div>
   )
